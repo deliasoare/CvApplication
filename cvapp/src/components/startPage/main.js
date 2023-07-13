66bab584823f8b1a751b2f56d092b8b61f425140
dristosun <107073369+dristosun@users.noreply.github.com>
2023-07-13 14:53:33 +0300

    Design and align start page

--- cvapp/src/components/startPage/main.js
+++ cvapp/src/components/startPage/main.js
@@ -2,8 +2,10 @@
 export default function StartPage({ onButtonClick }) {
     return (
         <div className="startPage">
-            <h1 className="startPageText">Create your perfect CV today!</h1>
-            <button onClick={onButtonClick} className="startPageButton">START</button>
+            <h1 className="startPageText">Create your perfect CV today with our app!</h1>
+            <div class="startPageButton"onClick={onButtonClick} className="startPageButton">
+                <a>START</a>
+            </div>
         </div>
     )
 }
